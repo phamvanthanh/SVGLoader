0f9625c6aca2dbcdb2530554ef5f9d4c21892f04
thanhpv <pthanh.ce@gmail.com>
2018-02-24 17:09:57 +0700

    svgColor(...) function

--- src/svgloader/SVGColor.java
+++ src/svgloader/SVGColor.java
@@ -1,32 +1,63 @@
 package svgloader;
 
+import javafx.scene.paint.Color;
 
 public class SVGColor {
     /**
     * Constructor
     */
-    public SVGColor( )
+    public SVGColor( ) {
+    	
+    }
     /**
     setOpacity sets generally the converted color to this Opacity setting
     @param opacity double, the opacity grad between 0.0 ... 1.0 (None...Full Transparent)
     */
-    public void setOpacity(double opacity)
+    public void setOpacity(double opacity) {
+    	
+    }
     /*
     getOpacity returns the Opacity setting
     @return double the setting opacity grad between 0.0 ... 1.0 (None...Full Transparent)
     */
-    public double getOpacity( )
+    public double getOpacity( ) {
+		return 0;
+    	
+    }
     /**
     svgColor converts a string to JavaFX color object
     @param color   string, lower case. E.g. white or #ffffff (6x hex)
     @return Color or null if string is invalid...
     */
-    public Color svgColor(String color)
+    public Color svgColor(String color) {
+		try {
+			return Color.web(color.toLowerCase());
+		}
+		catch (NullPointerException|IllegalArgumentException e){
+			System.out.println("Input color string is not valid");
+			e.printStackTrace();
+			return Color.BLACK;
+		}
+    	
+
+    }
     /**
     svgColor converts a string to JavaFX color object
     @param color   string, lower case. E.g. white or #ffffff (6x hex)
     @param opacity double, between 0.0 ... 1.0 (None...Full Transparent)
     @return Color or null if string is invalid...
     */
-    public Color svgColor(String color, double opacity)
+    public Color svgColor(String color, double opacity) {
+    	opacity = opacity > 1? 1:(opacity < 0? 0:opacity);
+    	
+    	try {
+    		return Color.web(color.toLowerCase(), opacity);
+		}
+		catch (NullPointerException|IllegalArgumentException e){
+			System.out.println("Color string is not valid");
+			e.printStackTrace();
+			return Color.BLACK;
+		}
+    	
+    }
 }
--- src/svgloader/TestSVGColor.java
+++ src/svgloader/TestSVGColor.java
@@ -1,9 +1,16 @@
 package svgloader;
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.stage.Stage;
 import svgloader.SVGColor;
+
 // More about SVG: see https://www.w3.org/TR/SVG/expanded-toc.html
 public class TestSVGColor extends Application {
-    public void start(Stage stage) throws Exception {
+	@Override
+	public void start(Stage stage) throws Exception {
         // load passing arguments
         Application.Parameters params = getParameters();
         java.util.List<String> pl = params.getRaw();
@@ -11,12 +18,18 @@ public class TestSVGColor extends Application {
 
         SVGColor svgColor = new SVGColor();
         Pane pane = new Pane();
-        Circle circle = new Circle(10, svgColor.svgColor("#0000ff"));
-        /* or with Opacity degree 0.5 (or 50%)
-        Circle circle = new Circle(10, svgColor.svgColor("blue", 0.5d));
-        */
+
+        Color c = svgColor.svgColor("red", 0.5);
+        System.out.println(c);
+        Circle circle = new Circle(100, c);
+        
+        circle.setLayoutX(100);
+        circle.setLayoutY(100);
+        
         pane.getChildren().add(circle);
         stage.setScene(new Scene(pane, Color.ANTIQUEWHITE));
        stage.show();
     }
+
+
 }
