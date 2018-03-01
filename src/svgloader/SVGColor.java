f42910841349030a6eaae42aadf9c06e150f22e1
Thanh Vu <ngthanh.vu94@gmail.com>
2018-03-01 17:17:11 +0700

    Reformat the code writing.

--- src/svgloader/SVGColor.java
+++ src/svgloader/SVGColor.java
@@ -6,24 +6,23 @@ public class SVGColor {
     /**
     * Constructor
     */
-    public SVGColor( ) {
-    	
+    public SVGColor() {
+
     }
     /**
     setOpacity sets generally the converted color to this Opacity setting
     @param opacity double, the opacity grad between 0.0 ... 1.0 (None...Full Transparent)
     */
     public void setOpacity(double value) {
-    	value = value > 1? 1:(value < 0? 0:value);
-    	opacity = value;
+      value = value > 1? 1:(value < 0? 0:value);
+      opacity = value;
     }
     /*
     getOpacity returns the Opacity setting
     @return double the setting opacity grad between 0.0 ... 1.0 (None...Full Transparent)
     */
     public double getOpacity( ) {
-		return opacity;
-    	
+      return opacity;
     }
     /**
     svgColor converts a string to JavaFX color object
@@ -31,7 +30,7 @@ public class SVGColor {
     @return Color or null if string is invalid...
     */
     public Color svgColor(String color) {
-		return svgColor(color, opacity);
+      return svgColor(color, opacity);
     }
     /**
     svgColor converts a string to JavaFX color object
@@ -39,18 +38,20 @@ public class SVGColor {
     @param opacity double, between 0.0 ... 1.0 (None...Full Transparent)
     @return Color or null if string is invalid...
     */
-    public Color svgColor(String color, double value) {
-    	
-    	  	
-    	try {
-			color = color.toLowerCase();
-			if(color.equals("none"))
-				return null;
-			return Color.web(color, opacity);			
-		}
-		catch (Exception e){} 
+    public Color svgColor(String color, double value)
+    {
+      try
+      {
+        color = color.toLowerCase();
+        if(color.equals("none"))
+        {
+          return null;
+        }
+        return Color.web(color, opacity);
+      }
+      catch (Exception e){}
     	return null;
-		   	
     }
-	private double opacity = 1;
+
+    private double opacity = 1;
 }
